c9c7aed8261467b49e9ba0b59683986b1ef8264a
Chen zhou <850054042@qq.com>
2022-05-06 10:37:50 +0800

    new

---
+++ src/ChessComponent.java
@@ -0,0 +1,66 @@
+import java.util.List;
+
+public abstract class ChessComponent {
+    //should design
+    private ChessboardPoint source;
+    private ChessColor chessColor;
+    protected char name;
+    private ChessComponent[][] chessComponents;
+    public ChessComponent[][] getChessBoard(){
+        return chessComponents;
+    }
+    public void setChessBoard(ChessComponent[][] board){
+        chessComponents = board;
+    }
+
+    //should design
+    public ChessComponent(){}
+
+    public ChessComponent(int x,int y){
+        source = new ChessboardPoint(x,y);
+    }
+
+    public void setChessColor(ChessColor chessColor) {
+        this.chessColor = chessColor;
+    }
+
+    // should design
+    public abstract List<ChessboardPoint> canMoveTo();
+
+    public int getX(){
+        return source.getX();
+    }
+
+    public int getY(){
+        return source.getY();
+    }
+
+    public void setX(int x){
+        source.setX(x);
+    }
+
+    public void setY(int y){
+        source.setY(y);
+    }
+
+    public ChessColor getChessColor() {
+        return chessColor;
+    }
+
+    public boolean isOpposite(ChessComponent chessComponent){
+        if(Character.isUpperCase(chessComponent.name)){
+            return Character.isLowerCase(name);
+        }
+        return Character.isUpperCase(name);
+    }
+
+    /**
+     * should design
+     * @return
+     */
+    @Override
+    public String toString() {
+        return String.valueOf(this.name);
+    }
+
+}
